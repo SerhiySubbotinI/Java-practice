b2cb88e90531112fa98281419e0dd585ace74897
SerhiySubbotinI <serhiysubbotin@gmail.com>
2022-08-04 12:16:11 +0300

    HomeWork1

---
+++ src/main/java/AlexHW1.java
@@ -0,0 +1,32 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.TreeSet;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class AlexHW1 {
+    public static void main(String[] args) {
+        var path = "src/main/Text.txt";
+
+        try {
+            var filePath = Path.of(path);
+            var sizeInMb = Double.valueOf(Files.size(filePath)) / (1024 * 1024);
+            var content = Files.readString(filePath);
+            var longWordsListSorted = Stream.of(content.split("[.,()\\[\\] \" \\?!: '\\n]"))
+                    .filter(s -> s.length() > 5)
+                    .map(String::toLowerCase)
+                    .collect(Collectors.toCollection(TreeSet::new));
+
+            System.out.printf("File size: %.5f Mb \n", sizeInMb);
+            System.out.printf("Words count: %d \n", longWordsListSorted.size());
+            System.out.printf("Sorted words: %s \n", String.join(", ", longWordsListSorted));
+
+        } catch (IOException e) {
+            System.out.printf(
+                    "ERROR - Failed to read file path: %s. Error message: %s",
+                    path, e.getLocalizedMessage()
+            );
+        }
+    }
+}
---
+++ src/main/java/HW1.java
@@ -0,0 +1,25 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.TreeSet;
+
+public class HW1 {
+    public static void main(String[] args) throws IOException {
+        String textFromFile = Files.readString(Paths.get("src/main/Text.txt"));
+        long sizeInBytes = textFromFile.getBytes().length;
+        int count = 0;
+        TreeSet<String> words = new TreeSet<>();
+        for (String word : textFromFile.toLowerCase().split("[^A-Za-zА-Яа-я]")) {
+            if (word.length() > 5) {
+                count++;
+                words.add(word);
+            }
+        }
+        System.out.printf("File size = %.6f Mb \n", (float) sizeInBytes / (1024 * 1024));
+        System.out.printf("Words in file = %d \n", count);
+        System.out.printf("All words in alphabetical order that are longer than 5 characters = %d \n", words.size());
+        System.out.println(words);
+    }
+}
+
+
